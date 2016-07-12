a2fe79429eba023e87bdb889c6a0730356a798c6
wisechengyi <wisechengyi@gmail.com>
2016-07-12 15:14:25 -0700

    python plugin update

--- scripts/prepare-ci-environment.sh
+++ scripts/prepare-ci-environment.sh
@@ -24,13 +24,13 @@ if [[ $IJ_ULTIMATE == "true" ]]; then
   export FULL_IJ_BUILD_NUMBER="IU-${IJ_BUILD_NUMBER}"
   export EXPECTED_IJ_MD5="e03c6d151c498891a47cd38b54b100b8"
   export PYTHON_PLUGIN_ID="Pythonid"
-  export PYTHON_PLUGIN_MD5="8ca536024429416f53df5e3eccaf0dc1"
+  export PYTHON_PLUGIN_MD5="553a68036a761a95d2b57637613e39df"
 else
   export IJ_BUILD="IC-${IJ_VERSION}"
   export FULL_IJ_BUILD_NUMBER="IC-${IJ_BUILD_NUMBER}"
   export EXPECTED_IJ_MD5="38f0387395647a874e0e103b48dd969d"
   export PYTHON_PLUGIN_ID="PythonCore"
-  export PYTHON_PLUGIN_MD5="cb55a98ab7ef78b87a6861591f6d368a"
+  export PYTHON_PLUGIN_MD5="c4e885fea86f5ad5da17a40804a3cafe"
 fi
 
 # we will use Community ids to download plugins.
